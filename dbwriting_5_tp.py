189ab6e4e5ca2ccfddbdb2f7487d5aba071a2aeb
florver <105130181+florver@users.noreply.github.com>
2022-05-18 16:52:05 -0300

    Update dbwriting_5_tp.py

--- dbwriting_5_tp.py
+++ dbwriting_5_tp.py
@@ -24,14 +24,14 @@ engine = psycopg2.connect(
 
 #### Tabla Product ####
 cursor = engine.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS base_TopProduct_Final (advertiser_id VARCHAR, fecha_act DATE, product_id VARCHAR, count INT);""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS base_TopProduct_Final (advertiser_id VARCHAR, fecha_act DATE, product_id VARCHAR);""")
 
 url='/home/ubuntu/grupo-kvd/TopProduct_final.csv'
 df = pd.read_csv(url)
 
 for i in range(0 ,len(df)):
-    values = (df['advertiser_id'][i] , df['date'][i],df['product_id'][i], df['count'][i].astype(int))
-    cursor.execute("INSERT INTO base_TopProduct_Final (advertiser_id, fecha_act, product_id, count) VALUES (%s, %s, %s, %s)",
+    values = (df['advertiser_id'][i] , df['date'][i],df['product_id'][i])
+    cursor.execute("INSERT INTO base_TopProduct_Final (advertiser_id, fecha_act, product_id) VALUES (%s, %s, %s)",
                   values)
 
 engine.commit()
